a14c82124cb17429f5deb7e0797c7d5d808f3c3c
knutc <knutc.kodehode@gmail.com>
2022-08-15 10:49:01 +0200

    xd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+import BigButton from "./components/BigButton";
 
 function App() {
   return (
     <div className="App">
       <Wrapper>
         <Title>styled component</Title>
+        <BigButton>Hello</BigButton>
       </Wrapper>
     </div>
   );
