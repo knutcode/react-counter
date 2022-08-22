6acff2ed05c8380b28b2d9288e0ba9320d494017
kodehodeknut <knutc.kodehode@gmail.com>
2022-08-22 19:04:44 +0200

    feature: counter using state and useEffect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,48 +3,81 @@ import Count from "../styled-components/Count";
 import { useState, useEffect } from "react";
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-  let [countUp, setCountUp] = useState(true);
+  // sets the state for count, bool, and pause timer
+  let [count, setCount] = useState(0);
+  let [bool, setBool] = useState(true);
+  let [pauseTimer, setPauseTimer] = useState(true);
 
-  //while
-  function plus() {
-    setCountUp(() => {
-      countUp = true;
-    });
-    console.log(`count up is ${countUp}`);
+  // increases count state by 1 and sets bool state to true
+  function increment() {
+    setCount(count + 1);
+    setBool(() => (bool = true));
   }
 
-  function minus() {
-    setCountUp(() => {
-      countUp = false;
-    });
-    console.log(`count up is ${countUp}`);
+  // decreases count state by 1 and sets bool state to false
+  function decrement() {
+    setCount(count - 1);
+    setBool(() => (bool = false));
   }
 
+  // toggles the pause timer (true => false || false => true)
+  function toggleTimer() {
+    setPauseTimer(() => !pauseTimer);
+  }
+
+  // | applies effect setTimeout() when selected states are updated ( [count, bool, pauseTimer] )
+  // | if bool is set to true, add +1 to count every 1000ms
+  // | if bool is set to false, subtract -1 to count every 1000ms instead
+  // |
+  // | clearTimeout prevents the timer from calling multiple timeouts f.ex if increment button is spammed (makes sure useEffect is ONLY called once every 1000ms)
+  // |
+  // V if pauserTimer is set to true, do nothing.
   useEffect(() => {
-    if (countUp === true)
-      setTimeout(() => {
-        setCount(function (prevCount) {
-          return prevCount + 1;
-        });
-      }, 1000);
-    else if (countUp === false)
-      setTimeout(() => {
-        setCount(function (prevCount) {
-          return prevCount - 1;
-        });
-      }, 1000);
-  });
+    const timer = setTimeout(() => {
+      if (pauseTimer) {
+        return;
+      }
+      if (bool) {
+        setCount((c) => c + 1);
+      } else if (!bool) {
+        setCount((c) => c - 1);
+      }
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [count, bool, pauseTimer]);
+
+  // resets the count to 0 and pauses the timer.
+  function reset() {
+    setCount((count = 0));
+    setPauseTimer((pauseTimer = true));
+  }
 
   return (
     <>
-      <Count>{count}</Count>
-      <SmallButton className="increase" onClick={plus}>
-        Increase
+      <Count>
+        {count}
+        {/* if the timer is't paused, adds "seconds" to the count, suggesting its counting in seconds. */}
+        {!pauseTimer ? "s" : ""}
+      </Count>
+
+      {/* visualizing that the counter is incrementing */}
+      <SmallButton className={!pauseTimer && bool ? "increase" : ""} onClick={increment}>
+        increment
       </SmallButton>
-      <SmallButton className="decrease" onClick={minus}>
-        Decrease
+
+      {/* visualizing that the counter is decrementing */}
+      <SmallButton className={!pauseTimer && !bool ? "decrease" : ""} onClick={decrement}>
+        decrement
       </SmallButton>
+
+      {/* toggles the timer on or off, and adds some styling */}
+      <SmallButton onClick={toggleTimer} className={pauseTimer ? "" : "stop"}>
+        {pauseTimer ? "start timer" : "stop timer"}
+      </SmallButton>
+
+      <br />
+      {/* resets the counter */}
+      <p onClick={reset}>reset counter</p>
     </>
   );
 };
--- src/styled-components/SmallButton.js
+++ src/styled-components/SmallButton.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 const SmallButton = styled.button`
   font-size: 24px;
   border-style: none;
-  background-color: lightblue;
   padding: 0.5em 1em;
   margin: 0.5em;
   border-radius: 5px;
